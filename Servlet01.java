03d9218f0b510d22da633a76f5633336e08af27f
cjy-CJY-cjy <3563022977@qq.com>
2024-09-19 16:27:30 +0800

    Add files via upload

---
+++ Servlet01.java
@@ -0,0 +1,38 @@
+package demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet01
+ */
+@WebServlet("/Servlet01")
+public class Servlet01 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//设置请求对象的属性
+		request.setAttribute("name", "jack");
+		request.setAttribute("psw", "123456");
+		//请求转发
+		request.getRequestDispatcher("jsp06.jsp")
+		.forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 
+		doGet(request, response);
+	}
+
+}
